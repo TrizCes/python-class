0a19864a23376ee93e08ac53fe5c9af863b66aa3
Beatriz Ceschini <biaceschini@gmail.com>
2024-09-02 23:35:23 -0300

    initial created

---
+++ FirstPython.py
@@ -0,0 +1,16 @@
+
+# x = 5;
+# y = 7;
+
+# # result = x + y;
+
+# # print(result);
+
+# Entrada de dados
+nota1 = 8
+nota2 = 9
+
+# Calculando a média
+media = (nota1 + nota2) / 2
+# Imprimindo o resultado
+print("A media:", media)
